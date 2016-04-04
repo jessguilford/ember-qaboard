5889d521c37c0fa23c942a35096abaad1ff21503
Jessie Guilford <Jessie Guilford>
2016-04-03 19:54:12 -0700

    fix delete answer issue

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    deleteAnswer() {
+    deleteAnswer(answer) {
       if (confirm('Are you sure you want to permanently delete this question?')) {
         this.sendAction('deleteAnswer', answer);
       }
--- app/components/question-page.js
+++ app/components/question-page.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
   actions: {
     delete(question) {
       if (confirm('Are you sure you want to permanently delete this question?')) {
-        this.sendAction('destroyQuestion', question);
+      this.sendAction('destroyQuestion', question);
       }
     },
     deleteAnswer(answer) {
