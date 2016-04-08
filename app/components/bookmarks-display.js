c4e1ced6ea8ea6a765738128ef8005d21a5c4106
Jessie Guilford <Jessie Guilford>
2016-04-08 14:13:20 -0700

    add basic bookmark service

--- app/components/bookmarks-display.js
+++ app/components/bookmarks-display.js
@@ -2,9 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   bookmarkQuestions: Ember.inject.service(),
-  actions: {
-    addToBookmarks(question) {
-      this.get('bookmarkQuestions').add(question);
-    },
-  },
 });
